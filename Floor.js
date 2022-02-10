7cac6d12dbd431c43564f3a92a465d980b9d273a
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-02-10 11:32:21 +0000

    collisions

--- Floor.js
+++ Floor.js
@@ -1,9 +1,9 @@
-class Box {
+class Floor {
   constructor(x,y,w,h,options) {
     this.body = Bodies.rectangle(x,y,w,h,options);
     this.w = w;
     this.h = h;
-    this.label = options;
+    this.options = options;
     World.add(world, this.body)
   }
   
@@ -11,6 +11,7 @@ class Box {
   show() {
     let pos = this.body.position;
     let angle = this.body.angle;
+    let options = this.options
 
     push();
     translate(pos.x, pos.y);
--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ let world;
 let boxes = [];
 let ground;
 
+let hp = 1;
+
 function setup() {
   createCanvas(400, 400);
   engine = Engine.create();
@@ -16,12 +18,24 @@ function setup() {
   let options = {
     isStatic: true
   }
-  ground = Bodies.rectangle(200, height, width, options);
+ 
+  ground = new Floor(200, height, width, 10, options);
   World.add(world, ground);
+
+  Matter.Events.on(engine, "collisionStart", 
+  (event) => {console.log(event.pairs[0].bodyA);
+    console.log(event.pairs[0].bodyB);
+    World.remove(world, event.pairs[0].bodyB)
+  boxes.pop();}
+  );
+
 }
 
 function mousePressed() {
-  boxes.push(new Box(mouseX, mouseY, 50, 50));
+  let options = {
+    label: "box"
+  }
+  boxes.push(new Box(mouseX, mouseY, 50, 50, options));
 }
 
 function draw() {
@@ -29,5 +43,7 @@ function draw() {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].show();
   }
+
+  ground.show();
   
 }
